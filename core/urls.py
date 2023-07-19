66ed72b2713801ef59782eb53bb31a140a636cce
Asir Faysal <afaysal220@gmail.com>
2023-07-19 13:04:43 +0600

    Initial Push

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('signup',views.signup,name='signup'),
+    path('signin',views.signin,name='signin'),
+    path('logout', views.logoutUser,name='logout'),
+]
